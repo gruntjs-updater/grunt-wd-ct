3ec4075f4240e2c3a74404932c2b6b4f037bf342
sideroad <sideroad.jp@gmail.com>
2014-10-06 23:43:19 +0900

    Add fixture

---
+++ test/fixtures/b/interaction.js
@@ -0,0 +1,20 @@
+module.exports = function(wd){
+	'use strict';
+	return {
+		input: {
+			'open': function(url){
+				return this.get(url);
+			}
+		},
+		assertion: {
+			'should have title "sideroad - Rich Web Application Development"': function(){
+				return this.title()
+				           .should.eventually.equal('sideroad - Rich Web Application Development');
+			},
+			'should have title "sideroad (sideroad)"': function(){
+				return this.title()
+				           .should.eventually.equal('sideroad (sideroad) · GitHub');
+			}
+		}
+	};
+};
